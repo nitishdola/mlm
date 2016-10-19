794b3aaa959a27fecaf9938a30551b7ef2c7fd85
Nitish Dolakasharia <nitish.dola@gmail.com>
2016-10-19 15:44:43 +0530

    drop down issue navbar solved

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         DB::table('admins')->insert(['username' => 'admin', 'password' => bcrypt('mlmAdmin#')]);
+        // DB::table('admins')->insert(['username' => 'admin', 'password' => bcrypt('mlmAdmin#')]);
+
+         DB::table('districts')->insert(['name' => 'Barpeta']);
+         DB::table('districts')->insert(['name' => 'Bongaigaon']);
+         DB::table('districts')->insert(['name' => 'Cachar']);
+         DB::table('districts')->insert(['name' => 'Darrang']);
+         DB::table('districts')->insert(['name' => 'Dhemaji']);
+         DB::table('districts')->insert(['name' => 'Dhubri']);
+         DB::table('districts')->insert(['name' => 'Dibrugarh']);
+         DB::table('districts')->insert(['name' => 'Goalpara']);
+         DB::table('districts')->insert(['name' => 'Golaghat']);
+         DB::table('districts')->insert(['name' => 'Hailakandi']);
+         DB::table('districts')->insert(['name' => 'Jorhat']);
+         DB::table('districts')->insert(['name' => 'Karbi Anglong']);
+         DB::table('districts')->insert(['name' => 'Karimganj']);
+         DB::table('districts')->insert(['name' => 'Kokrajhar']);
+         DB::table('districts')->insert(['name' => 'Lakhimpur']);
+         DB::table('districts')->insert(['name' => 'Morigaon']);
+         DB::table('districts')->insert(['name' => 'Nagaon']);
+         DB::table('districts')->insert(['name' => 'Nalbari']);
+
+         DB::table('districts')->insert(['name' => 'Dima Hasao']);
+         //DB::table('districts')->insert(['name' => 'Sivasagar']);
+         DB::table('districts')->insert(['name' => 'Sonitpur']);
+         DB::table('districts')->insert(['name' => 'Tinsukia']);
+         DB::table('districts')->insert(['name' => 'Kamrup']);
+         DB::table('districts')->insert(['name' => 'Baksa']);
+         DB::table('districts')->insert(['name' => 'Udalguri']);
+         DB::table('districts')->insert(['name' => 'Chirang']);
+         DB::table('districts')->insert(['name' => 'Majuli']);
     }
 }
--- resources/views/admin/common/sub_nav.blade.php
+++ resources/views/admin/common/sub_nav.blade.php
@@ -4,7 +4,7 @@
       <ul class="mainnav">
         <li @if(isset($class) && $class == 'active') class="active" @endif><a href="{{ route('admin.home')}}"><i class="icon-dashboard"></i><span>Dashboard</span> </a> </li>
         <!-- <li><a href="#"><i class="icon-list-alt"></i><span>Reports</span> </a> </li> -->
-        <li class="@if(isset($class) && $class == 'active') dropdown @endif"><a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown"> <i class="icon-long-arrow-down"></i><span>Customer</span> <b class="caret"></b></a>
+        <li class="dropdown @if(isset($class) && $class == 'active') active @endif"><a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown"> <i class="icon-long-arrow-down"></i><span>Customer</span> <b class="caret"></b></a>
           <ul class="dropdown-menu">
             <li><a href="{{ route('admin.create.user')}}">Add New Customer</a></li>
             <li><a href="{{ route('admin.user.index')}}">View All Customer</a></li>
